c6043cee4eda16188499670e341c7447b49ba39c
Lesley Cheung <lesley.yc@gmail.com>
2021-12-02 09:05:26 +0800

    Able to render the handlebars template in another file

--- frontend/public/js/pages/dashboard/index.js
+++ frontend/public/js/pages/dashboard/index.js
@@ -1,31 +1,23 @@
 var arr = ["get_all_problem", "show_all_problem", "get_one_problem", "show_one_problem", "get_all_task", "get_one_task", "get_all_cheatsheet", "show_all_cheatsheet", "get_one_cheatsheet", "show_one_cheatsheet", "get_all_tag_snippet", "show_one_tag_snippet", "get_one_tag_snippet", "show_all_tag_snippet", "post_problem", "post_task", "post_cheatsheet", "post_tag_snippet", "edit_problem_1", "edit_task_1", "edit_cheatsheet_1", "edit_tag_snippet_1", "delete_problem_1", "delete_cheatsheet_1", "delete_tag_snippet_1", "delete_task_1"]
 
-let setProblem = Handlebars.compile(`{{#each problem}}<div id='problem-{{id}}' data-id={{id}} data-user_id='{{user_id}}' data-hourglass_id='{{hourglass_id}}' class='each_problem'><br/>
- <span class='noDisplay' name='{{id}}'></span><br/>
- <span class='noDisplay' name='{{user_id}}'></span><br/>
- <span class='noDisplay' name='{{hourglass_id}}'></span><br/>
- <span class='each_problem_problem_span'>{{problem}}</span><br/>{{/each}}`)
+// let setProblem = Handlebars.compile(`{{#each problem}}<div id='problem-{{id}}' data-id={{id}} data-user_id='{{user_id}}' data-hourglass_id='{{hourglass_id}}' class='each_problem'><br/>
+//  <span class='noDisplay' name='{{id}}'></span><br/>
+//  <span class='noDisplay' name='{{user_id}}'></span><br/>
+//  <span class='noDisplay' name='{{hourglass_id}}'></span><br/>
+//  <span class='each_problem_problem_span'>{{problem}}</span><br/>{{/each}}`)
 
-// function getAll(type) {
-//     let string = _.upperFirst(type)
-//     let getTemplate = `set${string}`
-//     let element = $(`.show_${type}_all_div`)
-//     axios.get(`http://debugging.tools/api/get/${type}`).then((array) => {
-//         console.log("got the data")
-//         let getData = getTemplate(array.data)
-//         element.html(getData)
-//     })
-// }
-axios.get(`https://debugging.tools/api/get/problem`).then((array) => {
-    console.log("data", array)
-    let element = $(`.show_problem_all_div`)
-
-    let data = setProblem({
-        problem: array.data
+function getAllProblems(type) {
+    let element = $(`.show_${type}_all_div`)
+    axios.get(`https://debugging.tools/api/get/${type}`).then((array) => {
+        console.log("got the data")
+        let getData = setProblem({
+            problem: array.data
+        })
+        element.html(getData)
     })
-    element.html(data)
-    console.log(data)
-})
+}
+getAllProblems("problem")
+
 localStorage.setItem("user_id", 1)
 class PostObject {
     constructor(user_id, type, id) {
--- frontend/public/js/pages/dashboard/templates/problems.js
+++ frontend/public/js/pages/dashboard/templates/problems.js
@@ -1,21 +1,3 @@
- let setProblem2 = Handlebars.compile(`{{#each problem}}<div id='problem-{{id}}' data-id={{id}} data-user_id='{{user_id}}' data-hourglass_id='{{hourglass_id}}' class='each_problem'><br/>
- <span class='noDisplay' name='{{id}}'></span><br/>
- <span class='noDisplay' name='{{user_id}}'></span><br/>
- <span class='noDisplay' name='{{hourglass_id}}'></span><br/>
- <span class='each_problem_problem_span'>{{problem}}</span><br/>
- <span class='each_problem_whatshouldbe_span'>{{whatshouldbe}}</span><br/>
- <span class='each_problem_whatactuallyis_span'>{{whatactuallyis}}</span><br/>
- <span class='each_problem_hypothesis_span'>{{hypothesis}}</span><br/>
- <span class='each_problem_plan_span'>{{plan}}</span><br/>
- <span class='each_problem_created_span'>{{created}}</span><br/>
- <span class='each_problem_email_span'>{{email}}</span><br/>
- <span class='each_problem_public_span'>{{public}}</span><br/>
- <span class='each_problem_status_span'>{{status}}</span><br/>
- <span class='each_problem_importance_span'>{{importance}}</span><br/>
- <span class='each_problem_difficulty_span'>{{difficulty}}</span><br/>
- <span class='each_problem_start_span'>{{start}}</span><br/>
- <span class='each_problem_edit_span'>{{edit}}</span><br/>
- <span class='each_problem_end_span'>{{end}}</span><br/>
- <button class='each_edit_problem_btn' type='submit' data-id='{{id}}' data-user_id='{{user_id}}' data-hourglass_id='{{hourglass_id}}'>edit</button><br/>
- <button type='submit' class='each_delete_problem_btn' data-id='{{id}}' data-user_id='{{user_id}}' data-hourglass_id='{{hourglass_id}}'>delete</button><br/>
- {{/each}}`)
+let setProblem = Handlebars.compile("{{#each problem}}<span id='problem-{{id}}' data-id={{id}} data-user-id='{{user_id}}' data-hourglass-id='{{hourglass_id}}' class='each_problem'> <span class='each_problem_problem_span'>{{problem}}</span> <span class='noDisplay' name='{{id}}'></span> <span class='noDisplay' name='{{user_id}}'></span> <span class='noDisplay' name='{{hourglass_id}}'></span> <span class='each_problem_problem_span'>{{problem}}</span> <span class='each_problem_whatshouldbe_span'>{{whatshouldbe}}</span> <span class='each_problem_whatactuallyis_span'>{{whatactuallyis}}</span> <span class='each_problem_hypothesis_span'>{{hypothesis}}</span> <span class='each_problem_plan_span'>{{plan}}</span> <span class='each_problem_created_span'>{{created}}</span> <span class='each_problem_email_span'>{{email}}</span> <span class='each_problem_public_span'>{{public}}</span> <span class='each_problem_status_span'>{{status}}</span> <span class='each_problem_importance_span'>{{importance}}</span> <span class='each_problem_difficulty_span'>{{difficulty}}</span> <span class='each_problem_start_span'>{{start}}</span> <span class='each_problem_edit_span'>{{edit}}</span> <span class='each_problem_end_span'>{{end}}</span><button class='each_edit_problem_btn' type='submit' data-id='{{id}}' data-user-id='{{user_id}}' data-hourglass-id='{{hourglass_id}}'>edit</button><button type='submit' class='each_delete_problem_btn' data-id='{{id}}' data-user-id='{{user_id}}' data-hourglass-id='{{hourglass_id}}'>delete</button></span>{{/each}}")
+
+console.log(setProblem)
