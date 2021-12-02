c84d0624bae7c4eb3379fd6dcc2215296642ab0d
Lesley Cheung <lesley.yc@gmail.com>
2021-12-02 10:32:50 +0800

    lol

--- frontend/controller/pages/dashboard.js
+++ frontend/controller/pages/dashboard.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const router = express.Router()
 
-
 router.get("/dashboard", function(req, res) {
+    res.render("components/page/dashboard/layout", {
+        layout: "main"
+    })
+})
+
+router.get("/logic/dashboard", function(req, res) {
     res.render("pageLogic/dashboard/dashboard", {
         layout: "main"
     })
